3b46ad205a02074a668e1ad0b7712e9780e5cc15
sheilawangari <99507318+sheilawangari@users.noreply.github.com>
2022-07-22 15:31:26 +0300

    Band Name Generator

---
+++ newline.py
@@ -0,0 +1,3 @@
+print("Hello world\nHello world")     # Use \n to jump to the other line with no gaps, dont add a space
+
+print("Hello" + " " + "Sheila")   # String concatenation, adding separate characters and merging them into one
